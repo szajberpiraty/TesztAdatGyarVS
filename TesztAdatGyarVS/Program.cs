5e97ba222f8d24e006eeaa9ed6491377ea961227
szajberpiraty <szajberpiraty@gmail.com>
2018-10-14 18:34:03 +0200

    Minimalista verzió

--- TesztAdatGyarVS/Program.cs
+++ TesztAdatGyarVS/Program.cs
@@ -10,28 +10,79 @@ namespace TesztAdatGyarVS
 {
     class Program
     {
+        public static string[] csaladnevek = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\csaladnevek.txt"),Encoding.Default);
+
+        public static string[] ferfiak = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\ffi_keresztnevek.txt"),Encoding.Default);
+
+        public static string[] nok = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\noi_keresztnevek.txt"),Encoding.Default);
+
+        public static string[] helysegek = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\hu_telepulesek.txt"),Encoding.Default);
+        public static Random vsz = new Random();
+
         static void Main(string[] args)
         {
 
             //Path.Combine(Environment.CurrentDirectory, @"adatok\csaladnevek.txt");
-            string[] csaladnevek = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\csaladnevek.txt"));
-
-            string[] ferfiak = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\ffi_keresztnevek.txt"));
-
-            string[] nok = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\noi_keresztnevek.txt"));
-
-            string[] helysegek = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"adatok\hu_telepulesek.txt"));
 
 
+
+
+
+
+
+            List<TesztAdat> tesztadatok = new List<TesztAdat>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                TesztAdat tesztadat = new TesztAdat(vsz.Next(0, 2));
+                tesztadatok.Add(tesztadat);
+            }
+
+
+
+            //File.WriteAllLines(@"d:\razgon\tesztadatteszt.txt", tesztadatok.ToArray<TesztAdat>().ToString());
+
+            FileStream file = new FileStream(@"d:\razgon\tesztadatteszt.txt", FileMode.Create);
+            using (StreamWriter sw = new StreamWriter(file, Encoding.Default)) { 
+
+                foreach (var tesztadat in tesztadatok)
+                    {
+                    Console.WriteLine("{0},{1},{2},{3}", tesztadat.VezetekNev, tesztadat.KeresztNev, tesztadat.SzuletesEve, tesztadat.SzuletesiHely);
+                    sw.WriteLine(tesztadat.VezetekNev+","+tesztadat.KeresztNev+","+tesztadat.SzuletesEve+","+tesztadat.SzuletesiHely);
+                    }
+            };
 
 
             Console.WriteLine(csaladnevek.Length);
 
             Console.ReadLine();
         }
-        class Adatgyar
+
+        class TesztAdat
         {
+            public String VezetekNev { get; set; }
+            public String KeresztNev { get; set; }
+            public int SzuletesEve { get; set; }
+            public string SzuletesiHely { get; set; }
+
+            public TesztAdat(int nem)
+            {
+                VezetekNev = csaladnevek[vsz.Next(0, csaladnevek.Length)];
+                if (nem==0)
+                {
+                    KeresztNev = ferfiak[vsz.Next(0, ferfiak.Length)];
+                }else
+                {
+                    KeresztNev = nok[vsz.Next(0, nok.Length)];
+                }
+                
+                SzuletesEve = vsz.Next(1920, 2019);
+                SzuletesiHely = helysegek[vsz.Next(0, helysegek.Length)];
+
+
+            }
 
         }
     }
 }
+
